d962956888fbcfc774d85650bcdc2829a99d3791
Arjun Singh <arjun.singh981212@gmail.com>
2021-06-14 12:15:37 +0530

    Initial Commit

---
+++ src/main/java/org/arjun/CatalogueMicroservice/CatalogueMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.arjun.CatalogueMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueMicroserviceApplication.class, args);
+	}
+
+}
